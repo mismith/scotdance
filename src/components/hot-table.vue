56a2f4f081c1f3db64a1769650d743b4c2cc15a5
Murray Smith <murray@mismith.info>
2018-01-20 15:49:55 -0700

    substitute custom HotTable for official component
    …the official one doesn’t work because it isn’t exporting properly and requires a custom webpack build

---
+++ src/components/hot-table.vue
@@ -0,0 +1,29 @@
+<template>
+  <div />
+</template>
+
+<script>
+import Handsontable from 'handsontable';
+import 'handsontable/dist/handsontable.full.css';
+
+export default {
+  name: 'hot-table',
+  props: {
+    options: {
+      type: Object,
+      default: {},
+    },
+  },
+  data() {
+    return {
+      instance: undefined,
+    };
+  },
+  mounted() {
+    this.instance = new Handsontable(this.$el, this.options);
+  },
+  beforeDestroy() {
+    this.instance && this.instance.destroy();
+  },
+};
+</script>
