f9c177c72107829a2018232a52d7a9b28472233d
Murray Smith <murray@mismith.info>
2018-02-04 18:16:46 -0700

    improve handsontable custom renderer extending

--- src/helpers/handsontable.js
+++ src/helpers/handsontable.js
@@ -14,13 +14,13 @@ export function makeKeyValuePairColumn(column, valueProp = '$name', keyProp = id
         return this.getSourceDataAtRow(this.getSelected()[0])[keyProp];
       },
     },
-    renderer: function renderer(instance, td, row, col, prop, value) {
-      // eslint-disable-next-line prefer-rest-params
-      Handsontable.renderers.HandsontableRenderer.apply(this, arguments);
-
-      const item = column.source.find(i => i[keyProp] === value);
-      // eslint-disable-next-line no-param-reassign
-      if (item) td.innerHTML = item[valueProp];
+    renderer: function renderer(...args) {
+      const item = column.source.find(i => i[keyProp] === args[5]);
+      if (item) {
+        // eslint-disable-next-line no-param-reassign
+        args[5] = item[valueProp];
+      }
+      Handsontable.renderers.HandsontableRenderer.apply(this, args);
     },
   };
 }
