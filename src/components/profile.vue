9a660b9e17983a35780a7dd4f02ad04089123d3c
Murray Smith <murray@mismith.info>
2018-02-19 17:56:29 -0700

    add delete account functionality to profile page

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="profile md-scroll-frame">
     <div class="md-scroll">
-      <form class="md-padding">
+      <div v-if="me" class="md-padding">
         <md-avatar>
           <img
             :src="me.photoURL || `//avatars.io/gravatar/${me.email}`"
@@ -17,7 +17,50 @@
           <label>Email</label>
           <md-input v-model="me.email" readonly required />
         </md-field>
-      </form>
+
+        <footer>
+          <md-button @click="removeActive = true" class="md-accent">Delete Account</md-button>
+          <md-dialog :md-active.sync="removeActive" :md-fullscreen="false" class="remove-user-dialog">
+            <md-dialog-title>Are you sure?</md-dialog-title>
+
+            <md-dialog-content>
+              <p>This will permanently delete your account and all associated data.</p>
+              <p>In order to proceed, please enter your password:</p>
+              <md-field md-toggle-password>
+                <label>Password</label>
+                <md-input
+                  v-model="removeConfirm"
+                  type="password"
+                  name="password"
+                  @keypress.enter="remove"
+                />
+              </md-field>
+
+              <aside v-if="removeError" class="validation-message">
+                {{ removeError.message }}
+              </aside>
+            </md-dialog-content>
+
+            <md-dialog-actions>
+              <md-spinnable :md-spinning="removeLoading">
+                <md-button
+                  @click="remove"
+                  :disabled="!removeConfirm"
+                  class="md-primary md-raised"
+                >
+                  Delete Account
+                </md-button>
+              </md-spinnable>
+              <md-button @click="removeActive = false">Cancel</md-button>
+            </md-dialog-actions>
+          </md-dialog>
+        </footer>
+      </div>
+      <md-empty-state
+        v-else
+        md-icon="report_problem"
+        md-label="Not logged in"
+      />
     </div>
   </div>
 </template>
@@ -26,13 +69,54 @@
 import {
   mapState,
 } from 'vuex';
+import {
+  firebase,
+  idKey,
+  db,
+} from '@/helpers/firebase';
 
 export default {
   name: 'profile',
+  data() {
+    return {
+      removeActive: false,
+      removeConfirm: undefined,
+      removeLoading: false,
+      removeError: undefined,
+    };
+  },
   computed: {
     ...mapState([
       'me',
     ]),
+  },
+  watch: {
+    removeActive() {
+      this.removeConfirm = null;
+      this.removeError = null;
+    },
+  },
+  methods: {
+    async remove() {
+      this.removeLoading = true;
+      this.removeError = null;
+      if (this.me && this.removeConfirm) {
+        try {
+          const user = firebase.auth().currentUser;
+          await user.reauthenticateWithCredential(firebase.auth.EmailAuthProvider.credential(this.me.email, this.removeConfirm));
+          await user.delete();
+          await db.child('users').child(this.me[idKey]).remove();
+          await db.child('users:favorites ').child(this.me[idKey]).remove();
+
+          this.removeLoading = false;
+          this.removeActive = false;
+          this.$router.replace('/');
+        } catch (err) {
+          this.removeError = err;
+          this.removeLoading = false;
+        }
+      }
+    },
   },
 };
 </script>
