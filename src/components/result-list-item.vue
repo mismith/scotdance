8d6c99d07800d14758a8df945ef17b6cc3e2ea18
Murray Smith <murray@mismith.info>
2018-03-17 17:51:54 -0600

    add place component to result-list-item

--- src/components/result-list-item.vue
+++ src/components/result-list-item.vue
@@ -10,19 +10,30 @@
 
     <span class="md-list-item-text"><slot /></span>
 
-    <slot name="icon"><md-icon>chevron_right</md-icon></slot>
+    <slot name="icon">
+      <place v-if="place !== undefined" :place="place" />
+      <md-icon v-else>chevron_right</md-icon>
+    </slot>
   </md-list-item>
 </template>
 
 <script>
+import Place from '@/components/place';
+
 export default {
   name: 'result-list-item',
   props: {
     winner: Object,
+    place: Number,
+  },
+  components: {
+    Place,
   },
 };
 </script>
 
 <style lang="scss">
+.result-list-item {
 
+}
 </style>
