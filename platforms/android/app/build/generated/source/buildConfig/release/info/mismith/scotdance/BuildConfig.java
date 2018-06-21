3a79543804d2edf82e1ec5d066b0cd4d722b56ea
Murray Smith <murray@mismith.info>
2018-06-21 15:43:08 -0600

    1.2.2

--- platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "info.mismith.scotdance";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10201;
-  public static final String VERSION_NAME = "1.2.1";
+  public static final int VERSION_CODE = 10202;
+  public static final String VERSION_NAME = "1.2.2";
 }
