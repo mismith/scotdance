1b6f6eeded73bdec8a60cf1103457f5a550ab8d8
Murray Smith <murray@mismith.info>
2018-06-23 14:14:21 -0600

    replace schedule tabs with long-scrolling page

--- src/components/competition/schedule.vue
+++ src/components/competition/schedule.vue
@@ -1,53 +1,47 @@
 <template>
   <div class="competition-schedule blades md-scroll-frame alt">
     <transition :name="`slide-${currentEvent ? 'left' : 'right'}`">
-      <div v-if="!currentEvent" class="blade md-scroll-frame" key="list">
-        <md-tabs v-if="schedule.days" md-alignment="fixed">
-          <md-tab
-            v-for="(day, dayId) in schedule.days"
-            :key="dayId"
-            :md-label="$moment(day.date).format('ddd')"
-          >
-            <div v-persist-scroll="$route.fullPath" class="md-scroll-frame md-scroll">
-              <md-subheader v-if="day.name" class="md-title">
-                <span>{{ day.name }}</span>
-              </md-subheader>
+      <div v-if="!currentEvent" key="list" class="md-scroll-frame">
+        <div v-if="schedule.days" v-persist-scroll="$route.fullPath" class="md-scroll-frame md-scroll">
+          <div v-for="(day, dayId) in schedule.days" :key="dayId">
+            <md-subheader class="md-title">
+              <span>{{ day.name || $moment(day.date).format('dddd') }}</span>
+            </md-subheader>
 
-              <md-list class="md-list-cards">
-                <md-list-item-cards
-                  v-for="(block, blockId) in day.blocks"
-                  :key="blockId"
-                  md-expand
-                  :md-expanded="isBlockExpanded(blockId, Object.keys(day.blocks))"
-                  @toggled="handleBlockExpanded(blockId, $event)"
-                >
-                  <md-subheader>
-                    <div>{{ block.name }}</div>
-                    <div class="md-caption">{{ block.description }}</div>
-                  </md-subheader>
+            <md-list class="md-list-cards">
+              <md-list-item-cards
+                v-for="(block, blockId) in day.blocks"
+                :key="blockId"
+                md-expand
+                :md-expanded="isBlockExpanded(blockId, Object.keys(day.blocks))"
+                @toggled="handleBlockExpanded(blockId, $event)"
+              >
+                <md-subheader>
+                  <div>{{ block.name }}</div>
+                  <div class="md-caption">{{ block.description }}</div>
+                </md-subheader>
 
-                  <md-list slot="md-expand" class="md-double-line">
-                    <md-list-item
-                      v-for="(event, eventId) in block.events"
-                      :key="eventId"
-                      :to="{ params: { dayId, blockId, eventId } }"
-                      :class="{ active: isActive(dayId, blockId, eventId) }"
-                    >
-                      <div class="md-list-item-text">
-                        <div>{{ event.name }}</div>
-                        <div>{{ event.description }}</div>
-                      </div>
-                      <md-icon v-if="event.dances">chevron_right</md-icon>
-                    </md-list-item>
-                    <md-list-item v-if="!block.events" class="empty">
-                      No events found.
-                    </md-list-item>
-                  </md-list>
-                </md-list-item-cards>
-              </md-list>
-            </div>
-          </md-tab>
-        </md-tabs>
+                <md-list slot="md-expand" class="md-double-line">
+                  <md-list-item
+                    v-for="(event, eventId) in block.events"
+                    :key="eventId"
+                    :to="{ params: { dayId, blockId, eventId } }"
+                    :class="{ active: isActive(dayId, blockId, eventId) }"
+                  >
+                    <div class="md-list-item-text">
+                      <div>{{ event.name }}</div>
+                      <div>{{ event.description }}</div>
+                    </div>
+                    <md-icon v-if="event.dances">chevron_right</md-icon>
+                  </md-list-item>
+                  <md-list-item v-if="!block.events" class="empty">
+                    No events found.
+                  </md-list-item>
+                </md-list>
+              </md-list-item-cards>
+            </md-list>
+          </div>
+        </div>
         <md-empty-state
           v-else
           md-icon="clear"
@@ -205,11 +199,6 @@ export default {
 
 <style lang="scss">
 .competition-schedule {
-  .md-tabs {
-    .md-tabs-content {
-      background: initial;
-    }
-  }
   .admin-platforms {
     margin-bottom: 4px;
     overflow-x: auto;
