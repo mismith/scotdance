dd3ca80f86fef9dfe2e372f9ed426d8176d01595
Murray Smith <murray@mismith.info>
2018-06-27 23:31:47 -0600

    1.4.0

--- platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "info.mismith.scotdance";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10302;
-  public static final String VERSION_NAME = "1.3.2";
+  public static final int VERSION_CODE = 10400;
+  public static final String VERSION_NAME = "1.4.0";
 }
--- platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
+++ platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-statusbar": "2.4.2",
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-splashscreen": "5.0.2"
 };
 // BOTTOM OF METADATA
 });
