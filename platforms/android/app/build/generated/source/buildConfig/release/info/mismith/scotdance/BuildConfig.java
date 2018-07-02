34b6502c5f6ed6b083fe47aa717b2961bd864773
Murray Smith <murray@mismith.info>
2018-07-02 13:45:59 -0600

    1.4.4

--- platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/info/mismith/scotdance/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "info.mismith.scotdance";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10403;
-  public static final String VERSION_NAME = "1.4.3";
+  public static final int VERSION_CODE = 10404;
+  public static final String VERSION_NAME = "1.4.4";
 }
