3e425928b3e188dfdac12d4f1c5214bb4b768b5e
Murray Smith <murray@mismith.info>
2018-07-14 20:38:16 -0600

    replace avatars.io with gravatar

--- src/components/utility/gravatar.vue
+++ src/components/utility/gravatar.vue
@@ -1,16 +1,23 @@
 <template>
   <img
-    :src="user.photoURL || `https://avatars.io/gravatar/${encodeURIComponent(user.email)}`"
+    :src="user.photoURL || `https://gravatar.com/avatar/${hash}`"
     :alt="user.displayName"
     class="gravatar"
   />
 </template>
 
 <script>
+import md5 from 'md5';
+
 export default {
   name: 'gravatar',
   props: {
     user: Object,
+  },
+  computed: {
+    hash() {
+      return md5((this.user.email || '').toLowerCase());
+    },
   },
 };
 </script>
