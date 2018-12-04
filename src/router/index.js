afe2a2e0d0b536a0633f379f4ed75966bb39373f
Murray Smith <murray@mismith.info>
2018-12-03 23:37:00 -0700

    add default child route for competition.admin.info -> general

--- src/router/index.js
+++ src/router/index.js
@@ -107,6 +107,10 @@ export default new Router({
                   },
                 },
                 {
+                  path: 'info',
+                  redirect: 'info/general',
+                },
+                {
                   path: 'info/:subtabId?',
                   name: 'competition.admin.info',
                   component: () => import(/* webpackChunkName: "admin" */ '@/components/competition/admin/Info.vue'),
