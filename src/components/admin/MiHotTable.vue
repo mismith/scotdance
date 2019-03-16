2529e5377714274d63ae9240ced2a72965a163ba
Murray Smith <murray@mismith.info>
2019-03-16 14:41:03 -0600

    make MiHotTable data immutable…
    …in order to prevent ghost rows from being appended in memory

--- src/components/admin/MiHotTable.vue
+++ src/components/admin/MiHotTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <HotTable :settings="augmentHot(settings)" :data="data" class="MiHotTable" />
+  <HotTable :settings="augmentHot(settings)" :data="clonedData" class="MiHotTable" />
 </template>
 
 <script>
@@ -18,6 +18,15 @@ export default {
     settings: Object,
     data: Array,
   },
+  computed: {
+    clonedData() {
+      const data = this.data || (this.settings && this.settings.data);
+      if (Array.isArray(data)) {
+        return [...data];
+      }
+      return data;
+    },
+  },
   methods: {
     augmentHot(settings = {}) {
       const augmentedSettings = augmentHot(settings);
@@ -35,17 +44,21 @@ export default {
             const aggregated = {};
             changes.forEach(([row, prop, , newVal]) => {
               const physicalRow = this.toPhysicalRow(row); // to account for sorting
-              const key = instance.data[physicalRow] && instance.data[physicalRow][idKey];
-              aggregated[`${key || db.push().key}/${prop.replace('.', '/')}`] = newVal || null;
+              const key = instance.clonedData[physicalRow] && instance.clonedData[physicalRow][idKey];
+              if (key || newVal) { // ignore if new row and no value entered
+                aggregated[`${key || db.push().key}/${prop.replace('.', '/')}`] = newVal || null;
+              }
             });
-            instance.$emit('change', aggregated);
+            if (Object.keys(aggregated).length) { // ignore if new row and no value entered
+              instance.$emit('change', aggregated);
+            }
           }
         };
         augmentedSettings.beforeRemoveRow = function beforeRemoveRow(index, amount) {
           const aggregated = {};
           for (let row = index; row < index + amount; row += 1) {
             const physicalRow = this.toPhysicalRow(row); // to account for sorting
-            const key = instance.data[physicalRow] && instance.data[physicalRow][idKey];
+            const key = instance.clonedData[physicalRow] && instance.clonedData[physicalRow][idKey];
             if (key) {
               aggregated[key] = null;
             }
