302fbdcef23e774ff05e994f84748da1823e3edb
Murray Smith <murray@mismith.info>
2020-03-07 11:42:59 -0700

    improve EmptyState padding + text centering

--- src/components/EmptyState.vue
+++ src/components/EmptyState.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="EmptyState d-flex flex-column align-center justify-center pb-6 ma-auto">
+  <div class="EmptyState d-flex flex-column align-center justify-center px-3 pb-6 ma-auto">
     <v-icon v-if="icon" color="grey" :size="200">{{ icon }}</v-icon>
     <div v-if="label" class="headline my-4">{{ label }}</div>
     <div v-if="description" class="subtitle-1">{{ description }}</div>
@@ -17,3 +17,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.EmptyState {
+  text-align: center;
+}
+</style>
