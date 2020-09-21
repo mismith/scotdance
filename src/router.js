3da7c9a3edd10f303609ef92faf155ec84812e67
Murray Smith <murray@mismith.info>
2020-09-20 21:46:17 -0600

    rename All Competitions to Browse Competitions for consistency

--- src/router.js
+++ src/router.js
@@ -64,7 +64,7 @@ export default new Router({
           name: 'competitions',
           component: CompetitionsList,
           meta: {
-            title: 'All Competitions',
+            title: 'Browse Competitions',
           },
         },
         {
