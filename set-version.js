2218af1bbfc98f744a08e62c423d9b74ab3e43cd
Murray Smith <murray@mismith.info>
2020-10-06 18:45:01 -0600

    auto-set android version code on set-version

--- set-version.js
+++ set-version.js
@@ -19,12 +19,15 @@ function setVersion(v = undefined, {
   const { version } = JSON.parse(pkg);
   const incrementPatch = (_, m, n, p) => `${m}.${n}.${Number.parseInt(p, 10) + 1}`;
   const V = v || version.replace(VERSION_REGEX, incrementPatch);
+  const code = V.split('.').map((n, i) => (i ? n.padStart(2, '0') : n)).join('');
 
   // do some very naive/brittle find-and-replaces
   const replacement = `$1"${V}"`;
   fs.writeFileSync(pkgFile, pkg.replace(/("version": )"[^"]+"/, replacement));
   fs.writeFileSync(pkgLockFile, pkgLock.replace(/("version": )"[^"]+"/, replacement));
-  fs.writeFileSync(androidFile, android.replace(/(versionName )"[^"]+"/, replacement));
+  fs.writeFileSync(androidFile, android
+    .replace(/(versionName )"[^"]+"/, replacement)
+    .replace(/(versionCode )\d+/, `$1${code}`));
   fs.writeFileSync(iosFile, ios.replace(/(MARKETING_VERSION = )[^;]+;/g, `$1${V};`));
 
   console.info(`${version} -> ${V}`); // eslint-disable-line no-console
