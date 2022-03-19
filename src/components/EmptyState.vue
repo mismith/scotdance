a2a0eb92c1a758bdbdfd4a228414e63c38d43911
Murray Rowan <murray@paper.co>
2022-03-19 01:07:56 -0600

    improve EmptyState padding

--- src/components/EmptyState.vue
+++ src/components/EmptyState.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="EmptyState d-flex flex-column align-center justify-center px-3 pb-6 ma-auto">
+  <div class="EmptyState d-flex flex-column align-center justify-center px-3 pt-4 pb-8 ma-auto">
     <v-icon v-if="icon" color="grey" :size="200">{{ icon }}</v-icon>
     <div v-if="label" class="headline my-4">{{ label }}</div>
     <div v-if="description" class="subtitle-1">{{ description }}</div>
