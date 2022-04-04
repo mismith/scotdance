11b74b53893fbc865642769a2ec4fd6ad74ecc19
Murray Rowan <murray@paper.co>
2022-04-04 01:30:04 -0600

    allow devving using prod data via new dev:production’ script

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -2,5 +2,5 @@ export function isCypress() {
   return Boolean(window.Cypress) || window.location.search.includes('cypress');
 }
 export function isDev() {
-  return process.env.NODE_ENV !== 'production' || isCypress();
+  return (process.env.NODE_ENV !== 'production' || isCypress()) && !window.location.search.includes('production');
 }
