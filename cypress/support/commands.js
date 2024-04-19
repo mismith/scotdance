d3672a304b5deff0c635683fb25f7328f4d4175e
Murray Rowan <murray@mismith.info>
2024-04-18 23:41:01 -0600

    getTest command accepts options + chains

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,8 +2,11 @@ import 'cypress-file-upload';
 
 import { firebase } from '../../src/helpers/firebase';
 
-Cypress.Commands.add('getTest', (id) => {
-  return cy.get(`[data-test-id="${id}"]`);
+Cypress.Commands.add('getTest', { prevSubject: 'optional' }, (subject, id, options = {}) => {
+  return cy.get(`[data-test-id="${id}"]`, {
+    withinSubject: subject,
+    ...options,
+  });
 });
 
 Cypress.Commands.add('auth', (method, args = []) => {
