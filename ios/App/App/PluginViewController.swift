14eddfc101eadd2191dfc7b3df20c6f84f114e06
Murray Rowan <murray@mismith.info>
2024-08-05 23:59:27 -0600

    first pass at upgrading capacitor

---
+++ ios/App/App/PluginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PluginViewController.swift
+//  App
+//
+//  Created by Murray Rowan on 2024-08-04.
+//
+
+import UIKit
+
+class PluginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
