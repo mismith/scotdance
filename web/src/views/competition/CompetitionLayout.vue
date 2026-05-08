4608b87a928dbd08dce9183b443696ee61d79f86
Murray Rowan <murray@mismith.info>
2026-05-08 13:59:40 -0600

    fix(web): pill-match hover bg on comp top-chrome action buttons

--- web/src/views/competition/CompetitionLayout.vue
+++ web/src/views/competition/CompetitionLayout.vue
@@ -93,12 +93,12 @@ const locationLabel = computed(() => competition.value?.location ?? '')
           <FavoriteCompetitionButton
             v-if="mode === 'info'"
             :competition-id="competitionId"
-            class="flex! size-9 items-center justify-center p-0! [view-transition-name:match-element]"
+            class="flex! size-9 items-center justify-center rounded-full! p-0! hover:bg-nav-foreground/10! [view-transition-name:match-element]"
           />
           <ShareButton
             :title="competition?.name ?? undefined"
             :text="competition?.name ?? undefined"
-            class="flex! size-9 items-center justify-center p-0! [view-transition-name:match-element]"
+            class="flex! size-9 items-center justify-center rounded-full! p-0! hover:bg-nav-foreground/10! [view-transition-name:match-element]"
           />
         </div>
       </div>
