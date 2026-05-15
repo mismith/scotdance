cabad5f326904e13df2c9ba98adf1e9af9b2bacc
Murray Rowan <murray@mismith.info>
2026-05-15 00:29:11 -0600

    style(web): reformat Policies whitespace

--- web/src/views/Policies.vue
+++ web/src/views/Policies.vue
@@ -3,10 +3,7 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div
-    class="flex flex-1 flex-col pb-[calc(var(--chrome-bottom)+1rem)]"
-    data-route="policies"
-  >
+  <div class="flex flex-1 flex-col pb-[calc(var(--chrome-bottom)+1rem)]">
     <header
       class="mx-auto flex w-full max-w-3xl items-start justify-between gap-3 p-4 pb-3"
     >
@@ -15,53 +12,64 @@ import { RouterLink } from 'vue-router'
       </div>
     </header>
 
-    <main
-      class="prose-policies mx-auto w-full max-w-3xl flex-1 space-y-6 p-4 pt-0"
-    >
+    <main class="prose-policies mx-auto w-full max-w-3xl flex-1 space-y-6 p-4 pt-0">
       <section id="privacy" class="space-y-4">
         <h2 class="text-2xl font-medium tracking-tight">Privacy Policy</h2>
 
         <p>
-          <em>ScotDance.app</em> uses enterprise-grade services and
-          industry-standard security practices for storing and communicating all
-          data. <em>ScotDance.app</em> does not share, sell, or advertise with
-          user data in any capacity.
+          <em>ScotDance.app</em> uses enterprise-grade services and industry-standard
+          security practices for storing and communicating all data.
+          <em>ScotDance.app</em> does not share, sell, or advertise with user data in any
+          capacity.
         </p>
 
         <p>
           <strong>
-            Any potentially user-identifying data gathered is used exclusively
-            for the purpose of improving the app, fixing bugs in the code,
-            providing support, etc.
+            Any potentially user-identifying data gathered is used exclusively for the
+            purpose of improving the app, fixing bugs in the code, providing support, etc.
           </strong>
         </p>
 
         <p>
-          <strong>If you navigate/use the site/app</strong>, common web traffic
-          stats are collected:
+          <strong>If you navigate/use the site/app</strong>, common web traffic stats are
+          collected:
         </p>
         <ul class="ml-6 list-disc space-y-1">
-          <li>basic analytics information, like: pageviews, IP addresses, dates and times</li>
-          <li>device-type information, e.g.: operating system, web browser name and version</li>
-          <li>information required by third-party services in order to function; for instance: cookies for determining login state</li>
+          <li>
+            basic analytics information, like: pageviews, IP addresses, dates and times
+          </li>
+          <li>
+            device-type information, e.g.: operating system, web browser name and version
+          </li>
+          <li>
+            information required by third-party services in order to function; for
+            instance: cookies for determining login state
+          </li>
         </ul>
 
         <p>
-          <strong>If you sign up for an account</strong>, the following user
-          data is stored:
+          <strong>If you sign up for an account</strong>, the following user data is
+          stored:
         </p>
         <ul class="ml-6 list-disc space-y-1">
           <li>your email address</li>
-          <li>your (public) display name, should you choose to set one in your profile</li>
+          <li>
+            your (public) display name, should you choose to set one in your profile
+          </li>
         </ul>
 
         <p>
-          <strong>If you submit a competition and/or edit competition data</strong>,
-          it is assumed that:
+          <strong>If you submit a competition and/or edit competition data</strong>, it is
+          assumed that:
         </p>
         <ul class="ml-6 list-disc space-y-1">
-          <li>this information is publicly shareable, given that you are explicitly entering into the system</li>
-          <li>any potentially sensitive information input is done so at your own discretion</li>
+          <li>
+            this information is publicly shareable, given that you are explicitly entering
+            into the system
+          </li>
+          <li>
+            any potentially sensitive information input is done so at your own discretion
+          </li>
         </ul>
 
         <hr class="border-border/60 my-6" />
@@ -77,7 +85,7 @@ import { RouterLink } from 'vue-router'
             visit the
             <RouterLink
               :to="{ name: 'profile' }"
-              class="underline underline-offset-4 hover:text-foreground"
+              class="hover:text-foreground underline underline-offset-4"
             >
               My Profile
             </RouterLink>
@@ -90,15 +98,16 @@ import { RouterLink } from 'vue-router'
         <hr class="border-border/60 my-6" />
 
         <p>
-          Additionally, a complete list of third-party products/services used
-          to administer <em>ScotDance.app</em> is available
+          Additionally, a complete list of third-party products/services used to
+          administer <em>ScotDance.app</em> is available
           <a
             href="https://github.com/mismith/scotdance#admin"
             target="_blank"
             rel="noopener"
-            class="underline underline-offset-4 hover:text-foreground"
+            class="hover:text-foreground underline underline-offset-4"
           >
-            on Github</a>.
+            on Github</a
+          >.
         </p>
       </section>
     </main>
